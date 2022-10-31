742c3a5c0faefdba9fdcaa9f25d90294da8ab337
Isaac Santiago <eoisaacc@gmail.com>
2022-10-31 18:09:13 -0300

    :lipstick: feat: set color pattern

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.tsx', './index.html'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Ubuntu', 'sans-serif'],
+      mono: ['Fira Code', 'monospace'],
+    },
+    extend: {
+      colors: {
+        'title-primary': 'rgb(var(--title-primary) / <alpha-value>)',
+        'text-primary': 'rgb(var(--text-primary) / <alpha-value>)',
+        'text-secondary': 'rgb(var(--text-secondary) / <alpha-value>)',
+
+        'base-lines': 'rgb(var(--base-lines) / <alpha-value>)',
+
+        'gradient-blue': 'rgb(var(--gradient-blue) / <alpha-value>)',
+        'gradient-green:': 'rgb(var(--gradient-green:) / <alpha-value>)',
+
+        'auxiliary-blue': 'rgb(var(--auxiliary-blue) / <alpha-value>)',
+        'auxiliary-green': 'rgb(var(--auxiliary-green) / <alpha-value>)',
+
+        'accent-orange': 'rgb(var(--accent-orange) / <alpha-value>)',
+        'accent-green': 'rgb(var(--accent-green) / <alpha-value>)',
+        'accent-red': 'rgb(var(--accent-red) / <alpha-value>)',
+        'accent-violet': 'rgb(var(--accent-violet) / <alpha-value>)',
+
+        'shape-primary': 'rgb(var(--shape-primary) / <alpha-value>)',
+        'shape-secondary': 'rgb(var(--shape-secondary) / <alpha-value>)',
+        'base-background': 'rgb(var(--base-background) / <alpha-value>)',
+      },
+      backgroundImage: {},
+    },
   },
   plugins: [],
 }
